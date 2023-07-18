b54714ba64aab3aeee4c4649d16fdb70598a54a6
NiPaccy <nikuzepacifique22@gmail.com>
2023-07-18 12:39:56 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,15 +7,15 @@
  */
 void print_alphabet_x10(void)
 {
-        int i;
-        int j;
-
-        for (i = 0; i < 10; i++)
-        {
-                for (j = 97; j <= 122; j++)
-                {
-                        _putchar(j);
-                }
-                _putchar('\n');
-        }
+	int i;
+	int j;
+	
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
 }
