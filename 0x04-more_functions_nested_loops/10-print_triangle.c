b73f2776b8583a277757a3dd03c76b51eb705915
NiPaccy <nikuzepacifique22@gmail.com>
2023-07-20 07:33:08 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_triangle - Prints a triangle using character '#' in the terminal
+ * @size: The size of the triangle
+ */
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
